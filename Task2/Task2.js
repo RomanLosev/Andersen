9d0aa71d14b0c533764e30f2f1c10d5427926380
RomanLosev <47692245+RomanLosev@users.noreply.github.com>
2019-09-04 18:14:57 +0300

    Add types and operators (#4)

---
+++ Task2/Task2.js
@@ -0,0 +1,30 @@
+
+alert( 8 / "2" );
+alert( 4 * "2" );
+alert( "a" + 7 );
+alert( "-5" - 2);
+
+alert( 5 % 2 );
+alert( 2 ** 3 );
+alert( '01' == 1 );
+
+let counter = 1;
+let a = ++counter; // (*)
+alert(a);
+
+let n = 2;
+n += 5;
+
+let str = "678";
+let num = Number(str);
+alert(typeof num);
+
+alert( 2 > 1 );
+alert( 2 != 1 );
+alert( 'á' > 'Á' );
+alert( '07' == 7 );
+
+
+
+
+
