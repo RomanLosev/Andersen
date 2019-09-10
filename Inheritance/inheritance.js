7123b8056dbe1fc21681854de0d01fb3d430e51c
RomanLosev <47692245+RomanLosev@users.noreply.github.com>
2019-09-10 17:36:57 +0300

    Add inheritance (#12)

---
+++ Inheritance/inheritance.js
@@ -0,0 +1,34 @@
+﻿//1. Есть объект a с набором свойств, объект b должен унаследовать свойства a  
+
+function Car (name) {
+    
+  this.name = name;
+    
+  this.year = 2015;
+  this.info = function () {
+   alert ("Имя: " + this.name + "; Год производства: " + this.year); 
+} 
+ 
+}   
+   
+
+
+var car = new Car("Honda");
+
+function Cabriolet (name) {
+    
+  this.name = name;
+
+}
+
+   Cabriolet.prototype = car;
+
+   white = new Cabriolet ("Mersedes");
+   
+   black = new Cabriolet ("BMW");
+
+   alert (white.year);
+
+   white.info();
+
+
