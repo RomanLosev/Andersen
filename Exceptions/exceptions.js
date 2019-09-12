8dcaf9adbea58a93eb75b0dc82f1c83787bb97b8
RomanLosev <47692245+RomanLosev@users.noreply.github.com>
2019-09-12 18:54:38 +0300

    Add exceptions (#13)

---
+++ Exceptions/exceptions.js
@@ -0,0 +1,50 @@
+﻿//1. Завернуть в try catch 1/0 и вывести на ноль делить нельзя 
+ 
+
+   try {
+
+  
+let result = 1/0;
+  
+   
+  if (result == Infinity) {
+   
+  throw new Error ("На ноль делить нельзя!");
+
+  }
+ 
+} 
+   catch (e) {
+  
+  
+  alert( 'Error: ' + e.message);
+  
+
+}
+
+// или
+
+   try {
+
+   
+let result = function(a,b) {
+
+    
+    if (b == 0) {
+   
+     throw new Error ("На ноль делить нельзя!");
+    
+    }   else {return a/b} ;   
+  
+   }  
+      
+     alert (result(15,0));
+ 
+
+
+}   catch (e) {
+  
+  
+   alert( 'Error: ' + e.message);
+
+}
