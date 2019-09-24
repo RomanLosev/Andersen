7876117268ce11092e74d0767bbb20005ec0fe8e
Roman Losev <r.loseu@andersenlab.com>
2019-09-24 10:09:51 +0300

    Add yandex test 24.09

---
+++ Tests_yandex_24_09/conf.js
@@ -0,0 +1,16 @@
+
+  exports.config = {
+    framework: 'jasmine',
+    directConnect:true,
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['test/spec.js'],
+    // SELENIUM_PROMISE_MANAGER: false,
+    capabilities: {
+      browserName: 'chrome'
+    },
+    jasmineNodeOpts: {
+      showColors: true,
+      defaultTimeoutInterval: 100000,
+      isVerbose: true
+    }
+  }
